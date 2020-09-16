1abd290ad13b854564ff6c67bf0ee15aead0be8d
Carlotadbm <cdebenitomoreno@yahoo.es>
2020-09-16 18:37:37 +0200

    Adding lessons 2 and 3

--- 2._Cleaning_text_data_I/initLesson.R
+++ 2._Cleaning_text_data_I/initLesson.R
@@ -2,6 +2,9 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+swirl_options(swirl_logging = TRUE)
+
 library(tidyverse)
 quijote <- "http://corpus.rae.es/frecCORDE/quijote1.TXT"
 quijote <- read_lines(quijote) 
