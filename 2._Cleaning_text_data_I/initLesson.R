b97a72717dae9e11c7816a3a35ba00a9ff2a6447
Carlotadbm <cdebenitomoreno@yahoo.es>
2020-09-16 18:15:01 +0200

    Adding the log for Student's Progress to lesson 1

--- 2._Cleaning_text_data_I/initLesson.R
+++ 2._Cleaning_text_data_I/initLesson.R
@@ -2,5 +2,8 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-
-swirl_options(swirl_logging = TRUE)
+library(tidyverse)
+quijote <- "http://corpus.rae.es/frecCORDE/quijote1.TXT"
+quijote <- read_lines(quijote) 
+Encoding(quijote) <- "latin1"
+quijote <- enc2utf8(quijote)
