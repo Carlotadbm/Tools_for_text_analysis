8f36d38ad3bd4ca9de1e57f49ac9077d6f6592d4
Carlota de Benito Moreno <cdebenitomoreno@yahoo.es>
2020-11-01 20:08:33 +0100

    Creating lessons 14&15
    Creating lessons 14&15. Also adapting name and organization in lessons 11-13.

---
+++ 14._Create_your_own_functions/scripts/function-correct.R
@@ -0,0 +1,11 @@
+#Copy the code that creates `read_europarl()` below.
+
+read_europarl <- function(file){
+  read_delim(file, delim = "\t", col_names = "text") %>% 
+    mutate(type = str_detect(text, "^<")) %>% 
+    mutate(type = ifelse(type == TRUE, "tag", "content")) %>% 
+    filter(!type == lag(type)) %>% 
+    mutate(index = seq_along(text)) %>% 
+    mutate(index = ceiling(index/2)) %>% 
+    spread(type, text)  
+}
