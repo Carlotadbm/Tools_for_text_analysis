6990c500ecfc23fcfc68bbed6c1e4357805cff00
Carlota de Benito Moreno <cdebenitomoreno@yahoo.es>
2020-11-02 23:03:42 +0100

    Update default argument in lag()

--- 14._Create_your_own_functions/scripts/function-correct.R
+++ 14._Create_your_own_functions/scripts/function-correct.R
@@ -4,7 +4,7 @@ read_europarl <- function(file){
   read_delim(file, delim = "\t", col_names = "text") %>% 
     mutate(type = str_detect(text, "^<")) %>% 
     mutate(type = ifelse(type == TRUE, "tag", "content")) %>% 
-    filter(!type == lag(type)) %>% 
+    filter(!type == lag(type, default = "content")) %>% 
     mutate(index = seq_along(text)) %>% 
     mutate(index = ceiling(index/2)) %>% 
     spread(type, text)  
