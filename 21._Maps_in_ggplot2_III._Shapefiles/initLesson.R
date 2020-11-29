b1b830c3469af4cc8a92af7d36bb90f6631266ce
Carlota de Benito Moreno <cdebenitomoreno@yahoo.es>
2020-11-29 12:32:15 +0100

    Creating lessons 21 & 22

---
+++ 21._Maps_in_ggplot2_III._Shapefiles/initLesson.R
@@ -0,0 +1,14 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+#read file kafi
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+kafi <- read_delim(file.path(.get_course_path(), "Tools_for_text_analysis", "21._Maps_in_ggplot2_III._Shapefiles", "Kafi_chdt.csv"), delim = "\t")
+
---
+++ 22._Maps_in_ggplot2_IV._Voronoi_diagrams/initLesson.R
@@ -0,0 +1,13 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+#read file kafi
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+subjects <- read_delim(file.path(.get_course_path(), "Tools_for_text_analysis", "22._Maps_in_ggplot2_IV._Voronoi_diagrams", "subjects_coser.csv"), delim = "\t")
