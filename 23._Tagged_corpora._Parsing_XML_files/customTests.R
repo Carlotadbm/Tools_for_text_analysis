c5c14fd1a48bfe4f631372564442249d2cb5928e
Carlota de Benito Moreno <cdebenitomoreno@yahoo.es>
2020-12-06 20:44:22 +0100

    Add lessons 23 and 24, remove redundancy from lesson 22

--- 23._Tagged_corpora._Parsing_XML_files/customTests.R
+++ 23._Tagged_corpora._Parsing_XML_files/customTests.R
@@ -26,7 +26,7 @@ getLog <- function(){
 submit_log <- function(){
    
    # Please edit the link below
-   pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSeLyImiwZSoeWlCpoljYYMbyxw2_bXh8tix6xMCeIl6jPnknQ/viewform?usp=pp_url"
+   pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSd4bdA6KI8lEbIiWyRXRM7PSYVXLyL1O0i5WKYsvDMX81IKGw/viewform?usp=pp_url"
    
    # Do not edit the code below
    if(!grepl("=$", pre_fill_link)){
